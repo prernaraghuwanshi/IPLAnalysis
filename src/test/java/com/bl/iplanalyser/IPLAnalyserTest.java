b1b33933c3a2a8c220d8eb9ed28e04823b770436
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-28 19:39:11 +0530

    [Prerna]ADDED: Test case to load Batsman and Bowler stats CSV file and return number of records

--- src/test/java/com/bl/iplanalyser/IPLAnalyserTest.java
+++ src/test/java/com/bl/iplanalyser/IPLAnalyserTest.java
@@ -1,13 +1,36 @@
 package com.bl.iplanalyser;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class IPLAnalyserTest {
     IPLAnalyser iplAnalyser;
+    private static final String BATTING_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String BOWLING_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostWkts.csv";
 
     @Before
     public void initialize() {
         iplAnalyser = new IPLAnalyser();
-        iplAnalyser.welcomeMessage();
     }
+
+    @Test
+    public void givenIPLBattingFile_whenLoaded_shouldGiveNumberOfRecords() {
+        try {
+            int numberOfRecords = iplAnalyser.loadBattingFile(BATTING_FILE_PATH);
+            Assert.assertEquals(101, numberOfRecords);
+        } catch (IPLAnalyserException e) {
+        }
+    }
+    @Test
+    public void givenIPLBowlingFile_whenLoaded_shouldGiveNumberOfRecords() {
+        try {
+            int numberOfRecords = iplAnalyser.loadBowlingFile(BOWLING_FILE_PATH);
+            Assert.assertEquals(99, numberOfRecords);
+        } catch (IPLAnalyserException e) {
+        }
+    }
+
+
+
 }
